f27866e173abd82bbad0f99fc681d301ea122157
Hendrik Kleikamp <h_klei18@uni-muenster.de>
2021-01-06 16:01:19 +0100

    clean up of example folder

--- examples/LDDMM/square_to_circle_example.py
+++ examples/LDDMM/square_to_circle_example.py
@@ -3,8 +3,8 @@ from pyLDDMM.utils.visualization import loadimg, saveimg, save_animation, plot_w
 
 if __name__ == "__main__":
     # load greyscale images
-    i0 = loadimg('./example_images/circle.png')
-    i1 = loadimg('./example_images/square.png')
+    i0 = loadimg('../example_images/circle.png')
+    i1 = loadimg('../example_images/square.png')
 
     problem = pyLDDMM.ImageRegistrationProblem(i0, alpha=1, gamma=1)
 
@@ -13,11 +13,11 @@ if __name__ == "__main__":
     im, v, energies, Phi0, Phi1, J0, J1, length = lddmm.register(i1, problem, sigma=0.1, epsilon=0.0001, K=50, return_all=True)
 
     # save i0 aligned to i1
-    saveimg('./example_images/out_s2c.png', im)
+    saveimg('../example_images/out_s2c.png', im)
 
     # save animation of the transformation
-    save_animation('./example_images/out_s2c.gif', J0)
+    save_animation('../example_images/out_s2c.gif', J0)
 
     # plot the transformation
     plt = plot_warpgrid(Phi1[0], interval=1)
-    plt.savefig('./example_images/out_s2c_warp.png')
+    plt.savefig('../example_images/out_s2c_warp.png')
--- examples/LDDMM/translation_example.py
+++ examples/LDDMM/translation_example.py
@@ -3,8 +3,8 @@ from pyLDDMM.utils.visualization import loadimg, saveimg, save_animation, plot_w
 
 if __name__ == "__main__":
     # load greyscale images
-    i0 = loadimg('./example_images/t0.png')
-    i1 = loadimg('./example_images/t1.png')
+    i0 = loadimg('../example_images/t0.png')
+    i1 = loadimg('../example_images/t1.png')
 
     problem = pyLDDMM.ImageRegistrationProblem(i1, alpha=1000, gamma=1)
 
@@ -13,14 +13,14 @@ if __name__ == "__main__":
     im, v, energies, Phi0, Phi1, J0, J1, length = lddmm.register(i0, problem, sigma=0.1, epsilon=0.01, K=15, return_all=True)
 
     # save i0 aligned to i1
-    saveimg('./example_images/out_translation.png', im)
+    saveimg('../example_images/out_translation.png', im)
 
     # save animation of the transformation
-    save_animation('./example_images/out_translation.gif', J0)
+    save_animation('../example_images/out_translation.gif', J0)
 
     # plot the transformation
     plt = plot_warpgrid(Phi1[0], interval=2)
-    plt.savefig('./example_images/out_translation_warp.png')
+    plt.savefig('../example_images/out_translation_warp.png')
 
     plt_inverse = plot_warpgrid(Phi0[-1], interval=2)
-    plt_inverse.savefig('./example_images/out_translation_warp_inverse.png')
+    plt_inverse.savefig('../example_images/out_translation_warp_inverse.png')
