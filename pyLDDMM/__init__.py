1c1f5d44e30051b35b29a3e45984b2999315f458
Hendrik Kleikamp <h_klei18@uni-muenster.de>
2021-03-05 12:49:12 +0100

    renamed LDDMM to lddmm

--- pyLDDMM/__init__.py
+++ pyLDDMM/__init__.py
@@ -1,4 +1,4 @@
-from .LDDMM import LDDMM
+from .lddmm import LDDMM
 from .geodesic_shooting import GeodesicShooting
 
 from .problems.image_registration_problem import ImageRegistrationProblem, ImageRegistrationProblemGS
