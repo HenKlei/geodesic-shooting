5bacd2e8c96820430525375e1c8570cb72e5d553
Hendrik Kleikamp <h_klei18@uni-muenster.de>
2021-03-11 14:30:38 +0100

    fixed bug in visualization of vector fields

--- geodesic_shooting/utils/visualization.py
+++ geodesic_shooting/utils/visualization.py
@@ -66,10 +66,9 @@ def plot_vector_field(vector_field, title='', interval=1, show_axis=False):
     if show_axis is False:
         axis.set_axis_off()
 
-    axis.invert_yaxis()
     axis.set_aspect('equal')
     axis.set_title(title)
 
-    axis.quiver(vector_field[0, ::interval, ::interval], vector_field[1, ::interval, ::interval])
+    axis.quiver(-vector_field[0, ::interval, ::interval], vector_field[1, ::interval, ::interval])
 
     return fig
