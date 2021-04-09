ba4a4ff928c0e0162e43961d906009d3af7166bc
Hendrik Kleikamp <h_klei18@uni-muenster.de>
2021-04-09 16:40:58 +0200

    adjusted examples for reduced geodesic shooting

--- examples/reduced_geodesic_shooting/square_to_circle_example.py
+++ examples/reduced_geodesic_shooting/square_to_circle_example.py
@@ -1,21 +1,33 @@
 import os
+import numpy as np
 import matplotlib.pyplot as plt
 
 import geodesic_shooting
-from geodesic_shooting.utils.io import load_image, save_image
+from geodesic_shooting.utils.io import save_image
 from geodesic_shooting.utils.visualization import plot_warpgrid, plot_vector_field
+from geodesic_shooting.utils.create_example_images import make_circle, make_square
 
 
 if __name__ == "__main__":
-    # load greyscale images
-    input_ = load_image('../example_images/square.png')
-    target = load_image('../example_images/circle.png')
+    # create images
+    target = make_circle(64, np.array([32, 32]), 20)
+    input_ = make_square(64, np.array([32, 32]), 40)
 
     # perform the registration
     geodesic_shooting = geodesic_shooting.GeodesicShooting(alpha=6., exponent=1)
     image, v0, energies, Phi0, length = geodesic_shooting.register(input_, target, sigma=0.1,
                                                                    epsilon=0.01, iterations=100,
                                                                    return_all=True)
+
+    norm = np.linalg.norm((target - image).flatten()) / np.linalg.norm(target.flatten())
+    print(f'Relative norm of difference: {norm}')
+
+    plt.matshow(input_)
+    plt.title("Input")
+    plt.matshow(target)
+    plt.title("Target")
+    plt.matshow(image)
+    plt.title("Result")
 
     FILEPATH_RESULTS = 'results/'
     if not os.path.exists(FILEPATH_RESULTS):
