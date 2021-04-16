e23f86413b1bb112b741c45e1ed82d8438637b37
Hendrik Kleikamp <h_klei18@uni-muenster.de>
2021-04-16 17:49:05 +0200

    use norm of reduced coordinates as regularization
    Furthermore, the register function can be interrupted now and this just leaves the for-loop.

--- examples/reduced_geodesic_shooting/different_translations_reuse_matrices.py
+++ examples/reduced_geodesic_shooting/different_translations_reuse_matrices.py
@@ -12,8 +12,8 @@ if __name__ == "__main__":
     # create images
     input_ = (make_circle(64, np.array([25, 40]), 18) * 0.2
               + make_circle(64, np.array([25, 40]), 15) * 0.8)
-    target = (make_circle(64, np.array([40, 25]), 18) * 0.2
-              + make_circle(64, np.array([40, 25]), 15) * 0.8)
+    target = (make_circle(64, np.array([35, 30]), 18) * 0.2
+              + make_circle(64, np.array([35, 30]), 15) * 0.8)
 
     rb_size = 1
     rb = None
@@ -24,13 +24,13 @@ if __name__ == "__main__":
                       title="Initial vector field (translation)", interval=2)
     plt.show()
 
-    reduced_gs = geodesic_shooting.ReducedGeodesicShooting(input_.shape, rb, alpha=1.,
+    reduced_gs = geodesic_shooting.ReducedGeodesicShooting(input_.shape, rb, alpha=1000.,
                                                            exponent=3,
                                                            precomputed_quantities=reduced_matrices)
 
     start = time.time()
     image, v0, energies, Phi0, length = reduced_gs.register(input_, target, sigma=0.1,
-                                                            epsilon=0.00001, iterations=10000,
+                                                            epsilon=0.1, iterations=10000,
                                                             return_all=True)
     end = time.time()
     reduced_registration_time = end - start
