d73eca4e2f7e09a0486856da82b828603568c72c
Hendrik Kleikamp <hendrik.kleikamp@uni-muenster.de>
2022-03-18 11:51:56 +0100

    fixed tests and examples

--- examples/geodesic_shooting/square_to_circle_example.py
+++ examples/geodesic_shooting/square_to_circle_example.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     input_ = make_square((64, 64), np.array([32, 32]), 40)
 
     # perform the registration
-    gs = geodesic_shooting.GeodesicShooting(alpha=6., exponent=2.)
+    gs = geodesic_shooting.GeodesicShooting(alpha=10., exponent=4.)
     result = gs.register(input_, target, sigma=0.01, return_all=True)
 
     plot_registration_results(result)
