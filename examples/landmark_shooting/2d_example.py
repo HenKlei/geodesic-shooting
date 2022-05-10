5609184c98860d0967ee56364d1c30bc86a6a0d4
Hendrik Kleikamp <hendrik.kleikamp@uni-muenster.de>
2022-05-10 10:15:29 +0200

    fixed indentation of 2d example landmark shooting file

--- examples/landmark_shooting/2d_example.py
+++ examples/landmark_shooting/2d_example.py
@@ -11,64 +11,61 @@ from geodesic_shooting.core import ScalarFunction
 from geodesic_shooting.utils.transformations import push_forward
 
 
-# +
-# define landmark positions
-input_landmarks = np.array([[5., 3.], [4., 2.], [1., 0.], [2., 3.]])
-target_landmarks = np.array([[6., 2.], [5., 1.], [1., -1.], [2.5, 2.]])
+if __name__ == "__main__":
+    # define landmark positions
+    input_landmarks = np.array([[5., 3.], [4., 2.], [1., 0.], [2., 3.]])
+    target_landmarks = np.array([[6., 2.], [5., 1.], [1., -1.], [2.5, 2.]])
 
-# perform the registration using landmark shooting algorithm
-gs = geodesic_shooting.LandmarkShooting()
-result = gs.register(input_landmarks, target_landmarks, sigma=0.1, return_all=True)
-final_momenta = result['initial_momenta']
-registered_landmarks = result['registered_landmarks']
+    # perform the registration using landmark shooting algorithm
+    gs = geodesic_shooting.LandmarkShooting()
+    result = gs.register(input_landmarks, target_landmarks, sigma=0.1, return_all=True)
+    final_momenta = result['initial_momenta']
+    registered_landmarks = result['registered_landmarks']
 
-# +
-# plot results
-plot_landmark_matchings(input_landmarks, target_landmarks, registered_landmarks)
+    # plot results
+    plot_landmark_matchings(input_landmarks, target_landmarks, registered_landmarks)
 
-plot_initial_momenta_and_landmarks(final_momenta.flatten(), registered_landmarks.flatten(),
-                                   min_x=0., max_x=7., min_y=-2., max_y=4.)
+    plot_initial_momenta_and_landmarks(final_momenta.flatten(), registered_landmarks.flatten(),
+                                       min_x=0., max_x=7., min_y=-2., max_y=4.)
 
-time_evolution_momenta = result['time_evolution_momenta']
-time_evolution_positions = result['time_evolution_positions']
-plot_landmark_trajectories(time_evolution_momenta, time_evolution_positions,
-                           min_x=0., max_x=7., min_y=-2., max_y=4.)
+    time_evolution_momenta = result['time_evolution_momenta']
+    time_evolution_positions = result['time_evolution_positions']
+    plot_landmark_trajectories(time_evolution_momenta, time_evolution_positions,
+                               min_x=0., max_x=7., min_y=-2., max_y=4.)
 
-ani = animate_landmark_trajectories(time_evolution_momenta, time_evolution_positions,
-                                    min_x=0., max_x=7., min_y=-2., max_y=4.)
+    ani = animate_landmark_trajectories(time_evolution_momenta, time_evolution_positions,
+                                        min_x=0., max_x=7., min_y=-2., max_y=4.)
 
-nx = 70
-ny = 60
-mins = np.array([0., -2.])
-maxs = np.array([7., 5.])
-spatial_shape = (nx, ny)
-flow = gs.compute_time_evolution_of_diffeomorphisms(final_momenta, input_landmarks,
-                                                    mins=mins, maxs=maxs, spatial_shape=spatial_shape)
-flow.plot("Flow")
+    nx = 70
+    ny = 60
+    mins = np.array([0., -2.])
+    maxs = np.array([7., 5.])
+    spatial_shape = (nx, ny)
+    flow = gs.compute_time_evolution_of_diffeomorphisms(final_momenta, input_landmarks,
+                                                        mins=mins, maxs=maxs, spatial_shape=spatial_shape)
+    flow.plot("Flow")
 
+    def set_landmarks_in_image(img, landmarks, sigma=1.):
+        x, y = np.meshgrid(np.linspace(mins[0], maxs[0], nx), np.linspace(mins[1], maxs[1], ny))
+        for i, l in enumerate(landmarks):
+            dst = (x - l[0])**2 + (y - l[1])**2
+            img += (i + 1.) * np.exp(-(dst.T / (2.0 * sigma**2)))
 
-def set_landmarks_in_image(img, landmarks, sigma=1.):
-    x, y = np.meshgrid(np.linspace(mins[0], maxs[0], nx), np.linspace(mins[1], maxs[1], ny))
-    for i, l in enumerate(landmarks):
-        dst = (x - l[0])**2 + (y - l[1])**2
-        img += (i + 1.) * np.exp(-(dst.T / (2.0 * sigma**2)))
+    sigma = gs.kernel.sigma
+    image = ScalarFunction(spatial_shape)
+    target_image = ScalarFunction(spatial_shape)
+    set_landmarks_in_image(image, input_landmarks, sigma=sigma)
+    set_landmarks_in_image(target_image, target_landmarks, sigma=sigma)
 
+    image.plot("Original image")
+    target_image.plot("Target image")
+    resulting_image = push_forward(image, flow)
+    resulting_image.plot("Transformed image")
+    plt.show()
 
-sigma = gs.kernel.sigma
-image = ScalarFunction(spatial_shape)
-target_image = ScalarFunction(spatial_shape)
-set_landmarks_in_image(image, input_landmarks, sigma=sigma)
-set_landmarks_in_image(target_image, target_landmarks, sigma=sigma)
-
-image.plot("Original image")
-target_image.plot("Target image")
-resulting_image = push_forward(image, flow)
-resulting_image.plot("Transformed image")
-plt.show()
-
-print(f"Input: {input_landmarks}")
-print(f"Target: {target_landmarks}")
-print(f"Result: {registered_landmarks}")
-rel_error = (np.linalg.norm(target_landmarks - registered_landmarks)
-             / np.linalg.norm(target_landmarks))
-print(f"Relative norm of difference: {rel_error}")
+    print(f"Input: {input_landmarks}")
+    print(f"Target: {target_landmarks}")
+    print(f"Result: {registered_landmarks}")
+    rel_error = (np.linalg.norm(target_landmarks - registered_landmarks)
+                 / np.linalg.norm(target_landmarks))
+    print(f"Relative norm of difference: {rel_error}")
