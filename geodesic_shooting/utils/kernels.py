094d5d6d2c70b8ff58506c0d5ffc1d288fbcf092
Hendrik Kleikamp <hendrik.kleikamp@uni-muenster.de>
2022-08-24 16:25:39 +0200

    extended Gaussian kernel to scalar-valued case

--- geodesic_shooting/utils/kernels.py
+++ geodesic_shooting/utils/kernels.py
@@ -9,29 +9,41 @@ class Kernel:
 
 class GaussianKernel(Kernel):
     """Class that implements a Gaussian (matrix-valued, diagonal) kernel."""
-    def __init__(self, sigma=1./np.sqrt(2.)):
+    def __init__(self, scalar=False, sigma=1./np.sqrt(2.)):
         """Constructor.
 
         Parameters
         ----------
+        scalar
+            If `True`, a scalar-valued kernel is returned, otherwise a matrix-valued,
+            diagonal kernel.
         sigma
             Scaling parameter for the Gaussian bell curve.
         """
         super().__init__()
         assert sigma > 0
+        self.scalar = scalar
         self.sigma = sigma
 
     def __call__(self, x, y):
         assert x.ndim == 1
         assert x.shape == y.shape
-        return np.exp(-np.linalg.norm(x-y)**2 / (self.sigma**2)) * np.eye(x.shape[0]) / (self.sigma**2)
+        res = np.exp(-np.linalg.norm(x-y)**2 / (self.sigma**2)) / (self.sigma**2)
+        if self.scalar:
+            return res
+        else:
+            return res * np.eye(x.shape[0])
 
     def derivative_1(self, x, y, i):
         """Derivative of kernel with respect to i-th component of x."""
         assert x.ndim == 1
         assert x.shape == y.shape
         assert 0 <= i < x.shape[0]
-        return 2. * (y[i] - x[i]) / (self.sigma**4) * self(x, y)[0][0]
+        res = 2. * (y[i] - x[i]) / self.sigma**4
+        if self.scalar:
+            return res * self(x, y)
+        else:
+            return res * self(x, y)[0][0]
 
     def derivative_2(self, x, y, i):
         """Derivative of kernel with respect to i-th component of y."""
