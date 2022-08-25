5c2a543ab3e600cce8d86c56c21beb589d6532ec
Hendrik Kleikamp <hendrik.kleikamp@uni-muenster.de>
2022-08-25 14:20:28 +0200

    changed way of computing average distance in test of landmark matching

--- tests/test_landmark_matching.py
+++ tests/test_landmark_matching.py
@@ -4,6 +4,13 @@ import geodesic_shooting
 
 
 def test_landmark_matching():
+    def compute_average_distance(target_landmarks, registered_landmarks):
+        dist = 0.
+        for x, y in zip(registered_landmarks, target_landmarks):
+            dist += np.linalg.norm(x - y)
+        dist /= registered_landmarks.shape[0]
+        return dist
+
     input_landmarks = np.array([[5., 3.]])
     target_landmarks = np.array([[6., 2.]])
 
@@ -11,8 +18,8 @@ def test_landmark_matching():
     result = gs.register(input_landmarks, target_landmarks, sigma=0.05, return_all=True,
                          landmarks_labeled=True)
     registered_landmarks = result['registered_landmarks']
-
-    assert np.linalg.norm(registered_landmarks - target_landmarks) < 1e-5
+    dist = compute_average_distance(target_landmarks, registered_landmarks)
+    assert dist < 1e-5
 
     input_landmarks = np.array([[5., 3.], [1., 1.]])
     target_landmarks = np.array([[6., 2.], [0., 0.]])
@@ -21,8 +28,8 @@ def test_landmark_matching():
     result = gs.register(input_landmarks, target_landmarks, sigma=0.05, return_all=True,
                          landmarks_labeled=True)
     registered_landmarks = result['registered_landmarks']
-
-    assert np.linalg.norm(registered_landmarks - target_landmarks) < 1e-5
+    dist = compute_average_distance(target_landmarks, registered_landmarks)
+    assert dist < 1e-5
 
     input_landmarks = np.array([[5., 3.], [1., 1.]])
     target_landmarks = np.array([[6., 2.], [0., 0.]])
@@ -31,8 +38,8 @@ def test_landmark_matching():
     result = gs.register(input_landmarks, target_landmarks, sigma=0.05, return_all=True,
                          landmarks_labeled=False,)
     registered_landmarks = result['registered_landmarks']
-
-    assert np.linalg.norm(registered_landmarks - target_landmarks) < 1e-3
+    dist = compute_average_distance(target_landmarks, registered_landmarks)
+    assert dist < 1e-3
 
     input_landmarks = np.array([[5., 3.], [4., 2.], [1., 0.], [2., 3.]])
     target_landmarks = np.array([[6., 2.], [5., 1.], [1., -1.], [2.5, 2.]])
@@ -42,5 +49,5 @@ def test_landmark_matching():
     result = gs.register(input_landmarks, target_landmarks, sigma=0.1, return_all=True,
                          landmarks_labeled=True)
     registered_landmarks = result['registered_landmarks']
-
-    assert np.linalg.norm(registered_landmarks - target_landmarks) < 5e-2
+    dist = compute_average_distance(target_landmarks, registered_landmarks)
+    assert dist < 5e-2
