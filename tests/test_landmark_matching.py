da47be40916e8d9a74bfb82ae18e5ac3934821f4
Hendrik Kleikamp <hendrik.kleikamp@uni-muenster.de>
2022-08-25 14:10:39 +0200

    extended test for landmark matching

--- tests/test_landmark_matching.py
+++ tests/test_landmark_matching.py
@@ -33,3 +33,14 @@ def test_landmark_matching():
     registered_landmarks = result['registered_landmarks']
 
     assert np.linalg.norm(registered_landmarks - target_landmarks) < 1e-3
+
+    input_landmarks = np.array([[5., 3.], [4., 2.], [1., 0.], [2., 3.]])
+    target_landmarks = np.array([[6., 2.], [5., 1.], [1., -1.], [2.5, 2.]])
+
+    # perform the registration using landmark shooting algorithm
+    gs = geodesic_shooting.LandmarkShooting(kwargs_kernel={'sigma': 2.})
+    result = gs.register(input_landmarks, target_landmarks, sigma=0.1, return_all=True,
+                         landmarks_labeled=True)
+    registered_landmarks = result['registered_landmarks']
+
+    assert np.linalg.norm(registered_landmarks - target_landmarks) < 5e-2
