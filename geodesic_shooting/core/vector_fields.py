added normalization to POD and fixed singular values

geodesic_shooting/core/vector_fields.py
@@ -238,7 +238,7 @@ class VectorField:
             return fig, axis
         return axis
 
-    def get_norm(self, order=None, restriction=np.s_[...]):
+    def get_norm(self, product_operator=None, order=None, restriction=np.s_[...]):
         """Computes the norm of the `VectorField`.
 
         Remark: If `order=None` and `self.dim >= 2`, the 2-norm of `self.to_numpy().ravel()`
@@ -246,15 +246,23 @@ class VectorField:
 
         Parameters
         ----------
+        product_operator
+            Operator with respect to which to compute the norm. If `None`, the standard l2-inner
+            product is used.
         order
             Order of the norm,
             see https://numpy.org/doc/stable/reference/generated/numpy.linalg.norm.html.
+        restriction
+            Slice that can be used to restrict the domain on which to compute the norm.
 
         Returns
         -------
         The norm of the `VectorField`.
         """
-        return np.linalg.norm(self.to_numpy()[restriction].flatten(), ord=order)
+        if product_operator:
+            return np.sqrt(product_operator(self).to_numpy()[restriction].flatten().dot(self.to_numpy()[restriction].flatten()))
+        else:
+            return np.linalg.norm(self.to_numpy()[restriction].flatten(), ord=order)
 
     norm = property(get_norm)
 

geodesic_shooting/utils/reduced.py
@@ -49,10 +49,11 @@ def pod(modes, num_modes=10, product_operator=None, return_singular_values='all'
         V = B.T.dot((V[:selected_modes] / S_pos[:, np.newaxis]).T)
         singular_vectors = np.real(V).T
         singular_vectors = [type_input(data=u.reshape(modes[0].full_shape)) + shift for u in singular_vectors]
+        singular_vectors = [u / u.get_norm(product_operator=product_operator) for u in singular_vectors]
         if return_singular_values == 'all':
-            return singular_vectors, np.real(all_singular_values)
+            return singular_vectors, np.real(all_singular_values)**2
         elif return_singular_values:
-            return singular_vectors, np.real(S)
+            return singular_vectors, np.real(S)**2
         else:
             return singular_vectors
     else:  # assuming L2-scalar product as default if no `product_operator` is provided