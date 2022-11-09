465e7f373db5bb3cb984c8d2f58c94e635ac973e
Hendrik Kleikamp <hendrik.kleikamp@uni-muenster.de>
2022-11-09 15:13:27 +0100

    fixed bug in version file

--- geodesic_shooting/version.py
+++ geodesic_shooting/version.py
@@ -1,6 +1,10 @@
+__version__ = '0.1.0'
 try:
     import git
-    repo = git.Repo(search_parent_directories=True)
-    __version__ = repo.head.object.hexsha
-except (ModuleNotFoundError, git.exc.InvalidGitRepositoryError):
-    __version__ = '0.1.0'
+    try:
+        repo = git.Repo(search_parent_directories=True)
+        __version__ = repo.head.object.hexsha
+    except git.exc.InvalidGitRepositoryError:
+        pass
+except ModuleNotFoundError:
+    pass
