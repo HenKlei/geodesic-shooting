395e496dbb8705bf40743d8f7877b041da54d9b8
Hendrik Kleikamp <hendrik.kleikamp@uni-muenster.de>
2022-11-09 14:58:43 +0100

    use git sha instead of version number

--- geodesic_shooting/version.py
+++ geodesic_shooting/version.py
@@ -1,1 +1,6 @@
-__version__ = '0.1.0'
+try:
+    import git
+    repo = git.Repo(search_parent_directories=True)
+    __version__ = repo.head.object.hexsha
+except (ModuleNotFoundError, git.exc.InvalidGitRepositoryError):
+    __version__ = '0.1.0'
