cdf691ae150d34fcd17dc5a47213798f383c376f
Hendrik Kleikamp <hendrik.kleikamp@uni-muenster.de>
2023-07-27 13:25:07 +0200

    fixed test for landmark matching

--- tests/test_landmark_matching.py
+++ tests/test_landmark_matching.py
@@ -3,20 +3,6 @@ import numpy as np
 import geodesic_shooting
 
 
-def test_differential_computation():
-    input_landmarks = np.array([[5., 3.], [4., 2.], [1., 0.], [2., 3.]])
-    target_landmarks = np.array([[6., 2.], [5., 1.], [1., -1.], [2.5, 2.]])
-
-    gs = geodesic_shooting.LandmarkShooting()
-
-    w = np.zeros(gs.dim)
-    for i in range(gs.dim):
-        ei = np.zeros(gs.dim)
-        ei[i] = 1.
-        w[i] = ((gs.DK(position) @ ei) @ momentum).dot(momentum)
-    assert np.allclose(w, (momentum.T @ gs.DK(position) @ momentum))
-
-
 def test_landmark_matching():
     def compute_average_distance(target_landmarks, registered_landmarks):
         dist = 0.
@@ -29,7 +15,27 @@ def test_landmark_matching():
     target_landmarks = np.array([[6., 2.]])
 
     gs = geodesic_shooting.LandmarkShooting()
-    result = gs.register(input_landmarks, target_landmarks, sigma=0.05, return_all=True,
+    result = gs.register(input_landmarks, target_landmarks, sigma=0.001, return_all=True,
+                         landmarks_labeled=True)
+    registered_landmarks = result['registered_landmarks']
+    dist = compute_average_distance(target_landmarks, registered_landmarks)
+    assert dist < 1e-5
+
+    input_landmarks = np.array([[5., 3.]])
+    target_landmarks = np.array([[6., 2.]])
+
+    gs = geodesic_shooting.LandmarkShooting()
+    result = gs.register(input_landmarks, target_landmarks, sigma=0.001, return_all=True,
+                         landmarks_labeled=True, optimization_method='newton')
+    registered_landmarks = result['registered_landmarks']
+    dist = compute_average_distance(target_landmarks, registered_landmarks)
+    assert dist < 1e-5
+
+    input_landmarks = np.array([[5., 3.], [1., 1.]])
+    target_landmarks = np.array([[6., 2.], [0., 0.]])
+
+    gs = geodesic_shooting.LandmarkShooting()
+    result = gs.register(input_landmarks, target_landmarks, sigma=0.001, return_all=True,
                          landmarks_labeled=True)
     registered_landmarks = result['registered_landmarks']
     dist = compute_average_distance(target_landmarks, registered_landmarks)
@@ -39,17 +45,7 @@ def test_landmark_matching():
     target_landmarks = np.array([[6., 2.], [0., 0.]])
 
     gs = geodesic_shooting.LandmarkShooting()
-    result = gs.register(input_landmarks, target_landmarks, sigma=0.05, return_all=True,
-                         landmarks_labeled=True)
-    registered_landmarks = result['registered_landmarks']
-    dist = compute_average_distance(target_landmarks, registered_landmarks)
-    assert dist < 1e-5
-
-    input_landmarks = np.array([[5., 3.], [1., 1.]])
-    target_landmarks = np.array([[6., 2.], [0., 0.]])
-
-    gs = geodesic_shooting.LandmarkShooting()
-    result = gs.register(input_landmarks, target_landmarks, sigma=0.05, return_all=True,
+    result = gs.register(input_landmarks, target_landmarks, sigma=0.001, return_all=True,
                          landmarks_labeled=False)
     registered_landmarks = result['registered_landmarks']
     dist = compute_average_distance(target_landmarks, registered_landmarks)
@@ -59,8 +55,18 @@ def test_landmark_matching():
     target_landmarks = np.array([[6., 2.], [5., 1.], [1., -1.], [2.5, 2.]])
 
     gs = geodesic_shooting.LandmarkShooting(kwargs_kernel={'sigma': .5})
-    result = gs.register(input_landmarks, target_landmarks, sigma=0.1, return_all=True,
+    result = gs.register(input_landmarks, target_landmarks, sigma=0.01, return_all=True,
                          landmarks_labeled=True)
+    registered_landmarks = result['registered_landmarks']
+    dist = compute_average_distance(target_landmarks, registered_landmarks)
+    assert dist < 1e-2
+
+    input_landmarks = np.array([[5., 3.], [4., 2.], [1., 0.], [2., 3.]])
+    target_landmarks = np.array([[6., 2.], [5., 1.], [1., -1.], [2.5, 2.]])
+
+    gs = geodesic_shooting.LandmarkShooting(kwargs_kernel={'sigma': .5})
+    result = gs.register(input_landmarks, target_landmarks, sigma=0.01, return_all=True,
+                         landmarks_labeled=True, optimization_method='newton')
     registered_landmarks = result['registered_landmarks']
     dist = compute_average_distance(target_landmarks, registered_landmarks)
     assert dist < 1e-2
