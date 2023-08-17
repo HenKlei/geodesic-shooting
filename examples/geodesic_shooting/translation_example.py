fixed gradient scaling and normalize gradient in descent method

examples/geodesic_shooting/translation_example.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
               + make_circle((64, 64), np.array([35, 25]), 12) * 0.8)
 
     # perform the registration
-    gs = geodesic_shooting.GeodesicShooting(alpha=0.01, exponent=2)
+    gs = geodesic_shooting.GeodesicShooting(alpha=0.1, exponent=1, gamma=1.)
     result = gs.register(template, target, sigma=0.01, return_all=True)
 
     result['initial_vector_field'].save_tikz('initial_vector_field_translation.tex',

geodesic_shooting/geodesic_shooting.py
@@ -59,7 +59,7 @@ class GeodesicShooting:
                 f"\tTime steps: {self.time_steps}\n"
                 f"\tSampler options: {self.sampler_options}")
 
-    def register(self, template, target, sigma=0.1, optimization_method='GD', optimizer_options={'disp': True},
+    def register(self, template, target, sigma=0.01, optimization_method='GD', optimizer_options={'disp': True},
                  initial_vector_field=None, restriction=np.s_[...], return_all=False, log_summary=True):
         """Performs actual registration according to LDDMM algorithm with time-varying vector
            fields that are chosen via geodesics.
@@ -225,7 +225,10 @@ class GeodesicShooting:
                                     message = 'maximum number of iterations reached'
                                     break
 
-                                d = -grad_x
+                                if norm_grad_x > 1:
+                                    d = -grad_x / norm_grad_x
+                                else:
+                                    d = -grad_x
                                 alpha = line_search(x, func_x, grad_x, d)
                                 x = x + alpha * d
                                 func_x, grad_x = func(x)

geodesic_shooting/utils/grad.py
@@ -33,7 +33,7 @@ def finite_difference(f):
         indices = list(range(dim))
         indices[0] = d
         indices[d] = 0
-        window_d = np.transpose(window / shape[d], axes=indices)
+        window_d = np.transpose(window, axes=indices) * shape[d]
         return correlate(u, window_d)
 
     derivatives = []